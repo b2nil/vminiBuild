11bca4d70a7329aa18fcd5c72f784234bbf85afe
Ken Ho <kentsu.ho@gmail.com>
2022-03-12 12:45:23 +0800

    chore: update template

--- template/project.config.ts
+++ template/project.config.ts
@@ -1,7 +1,7 @@
 export default defineProjectConfig({
   description: "项目配置文件",
   projectname: "vmini-build-demo",
-  appid: "touristid",
+  appid: "",
   compileType: "miniprogram",
   packOptions: {
     ignore: []
